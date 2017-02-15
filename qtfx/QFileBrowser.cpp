5a78f57312f2c6555f574295fdbfabf0b37dbc16
Thomas Geijtenbeek <tgeijten@gmail.com>
2017-02-15 15:45:01 +0100

    filebrowser fixes

--- qtfx/QFileBrowser.cpp
+++ qtfx/QFileBrowser.cpp
@@ -6,11 +6,24 @@ QFileBrowser::QFileBrowser( QWidget* parent ) : QTreeView( parent )
 	setModel( fileModel );
 	for ( int i = 1; i <= 3; ++i )
 		hideColumn( i );
+
+	connect( this, &QTreeView::activated, this, &QFileBrowser::activateItem );
+	connect( selectionModel(), &QItemSelectionModel::currentChanged, this, &QFileBrowser::selectItem );
 }
 
 void QFileBrowser::setFolder( const QString& folder, const QString& filter )
 {
 	QDir().mkdir( folder );
-	fileModel->setNameFilters( QStringList( "*.par" ) );
+	fileModel->setNameFilters( QStringList( filter ) );
 	setRootIndex( fileModel->setRootPath( folder ) );
+}
+
+void QFileBrowser::activateItem( const QModelIndex& idx )
+{
+	emit activated( fileModel->fileInfo( idx ) );
+}
+
+void QFileBrowser::selectItem( const QModelIndex& a, const QModelIndex& b )
+{
+	emit selectionChanged( fileModel->fileInfo( a ), fileModel->fileInfo( b ) );
 }
--- qtfx/QFileBrowser.h
+++ qtfx/QFileBrowser.h
@@ -17,7 +17,15 @@ public:
 	QFileSystemModel* fileSystemModel() { return fileModel; }
 
 public slots:
+
 signals:
+	void activated( QFileInfo filename );
+	void selectionChanged( QFileInfo current, QFileInfo previous );
+
+private slots:
+	void activateItem( const QModelIndex& idx );
+	void selectItem( const QModelIndex& a, const QModelIndex& b );
+
 
 protected:
 	QFileSystemModel* fileModel;
