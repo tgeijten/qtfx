5694723d12141892849dae6b808aef3239db0c7c
Thomas Geijtenbeek <tgeijten@gmail.com>
2019-03-25 14:01:22 +0100

    R: refactoring for new xo::string_cast implementation

--- qtfx/qt_convert.h
+++ qtfx/qt_convert.h
@@ -9,10 +9,3 @@
 inline QString to_qt( const std::string& s ) { return QString( s.c_str() ); }
 inline QString to_qt( const xo::path& s ) { return QString( s.string().c_str() ); }
 inline QColor to_qt( const xo::color& c ) { return QColor( 255 * c.r, 255 * c.g, 255 * c.b ); }
-
-namespace xo {
-	template<> struct string_cast<QString, void> {
-		static QString from( const string& value ) { return QString( value.c_str() ); }
-		static string to( const QString& s ) { return s.toStdString(); }
-	};
-}
